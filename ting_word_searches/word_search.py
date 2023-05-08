eadd37696c6cb81e8a80538243863ba79be3a3b8
Alex <alexsandrolima991@hotmail.com>
2023-05-08 19:13:58 -0300

    Início da implementação da função exists Word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,12 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    file_text = txt_importer(instance._files[0])
+
+    for line in file_text:
+        if word.lower() not in line.lower():
+            return []
 
 
 def search_by_word(word, instance):
