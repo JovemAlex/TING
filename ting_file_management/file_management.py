11266cd05df9944d449fbe6c31f77d105fa69f5c
Alex <alexsandrolima991@hotmail.com>
2023-05-08 16:19:00 -0300

    Implementa função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write('Formato inválido')
+
+        with open(path_file, "r") as txtfile:
+            file_content = txtfile.read().split("\n")
+
+            return file_content
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
