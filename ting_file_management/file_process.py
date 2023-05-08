a86a4c4aa7f81aafb57382c8510dbc3b44d4820b
Alex <alexsandrolima991@hotmail.com>
2023-05-08 17:54:36 -0300

    Implementa funções em file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,54 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file not in instance._files:
+        file_lines = txt_importer(path_file)
+        instance.enqueue(path_file)
+        value_enqueued = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file_lines),
+            "linhas_do_arquivo": file_lines,
+        }
+        print(f"{value_enqueued}", file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        files = instance._files
+        file_name = instance._files[0]
+
+        if not len(files):
+            print("Não há elementos", file=sys.stdout)
+
+        instance.dequeue()
+
+        print(
+            f"Arquivo {file_name} removido com sucesso",
+            file=sys.stdout,
+        )
+
+    except IndexError:
+        print("Não há elementos", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        files = instance._files
+        file_exact = files[position]
+
+        if position not in files:
+            print("Posição inválida", file=sys.stderr)
+
+        file_lines = txt_importer(file_exact)
+
+        meta = {
+            "nome_do_arquivo": file_exact,
+            "qtd_linhas": len(file_lines),
+            "linhas_do_arquivo": file_lines,
+        }
+
+        print(meta, file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
