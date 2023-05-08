39c627073d359ec1e270f5479ac904fae2b6b27a
Alex <alexsandrolima991@hotmail.com>
2023-05-08 20:33:52 -0300

    Complemento da função exists Word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,12 +1,23 @@
-from ting_file_management.file_management import txt_importer
-
-
 def exists_word(word, instance):
-    file_text = txt_importer(instance._files[0])
 
-    for line in file_text:
-        if word.lower() not in line.lower():
-            return []
+    message = list()
+    ocurrences = list()
+
+    for file in instance._files:
+
+        for index, linha in enumerate(file["linhas_do_arquivo"], start=1):
+            if word.lower() in linha.lower():
+                ocurrences.append({"linha": index + 1})
+
+        result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": ocurrences,
+        }
+
+        message.append(result)
+
+    return message
 
 
 def search_by_word(word, instance):
